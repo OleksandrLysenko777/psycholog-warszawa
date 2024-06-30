92e28750ebc017ffbd492e2640728190b8735e93
OleksandrLysenko777 <direst2010@gmail.com>
2024-06-30 16:13:26 +0200

    edited

--- src/components/Pages/StartPage/StartPage.jsx
+++ src/components/Pages/StartPage/StartPage.jsx
@@ -14,10 +14,14 @@ const ReviewCard = ({ review, specialist }) => {
   return (
     <div className="review-card">
       <div className="specialist-name">
-        <p><strong>{specialist.name}</strong></p>
+        <p>
+          <strong>{specialist.name}</strong>
+        </p>
       </div>
       <div className="review-header">
-        <p><strong>{review.name}</strong></p>
+        <p>
+          <strong>{review.name}</strong>
+        </p>
         <ReactStars
           count={5}
           value={review.rating}
@@ -38,13 +42,67 @@ const StartPage = ({ reviews, specialists }) => {
   const { t } = useTranslation();
 
   const sections = [
-     { id: 'section1', title: t('start.sections.section1.title'), content: t('start.sections.section1.content'), image: process.env.PUBLIC_URL + '/images/15.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section2', title: t('start.sections.section2.title'), content: t('start.sections.section2.content'), image: process.env.PUBLIC_URL + '/images/12.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section3', title: t('start.sections.section3.title'), content: t('start.sections.section3.content'), image: process.env.PUBLIC_URL + '/images/13.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section4', title: t('start.sections.section4.title'), content: t('start.sections.section4.content'), image: process.env.PUBLIC_URL + '/images/14.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section5', title: t('start.sections.section5.title'), content: t('start.sections.section5.content'), image: process.env.PUBLIC_URL + '/images/15.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section6', title: t('start.sections.section6.title'), content: t('start.sections.section6.content'), image: process.env.PUBLIC_URL + '/images/12.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
-    { id: 'section7', title: t('start.sections.section7.title'), content: t('start.sections.section7.content'), image: process.env.PUBLIC_URL + '/images/14.jpg', icon: process.env.PUBLIC_URL + '/images/icon.png' },
+    {
+      id: 'section1',
+      title: t('start.sections.section1.title'),
+      content: [t('start.sections.section1.content')],
+      image: process.env.PUBLIC_URL + '/images/15.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section2',
+      title: t('start.sections.section2.title'),
+      content: [t('start.sections.section2.content')],
+      image: process.env.PUBLIC_URL + '/images/12.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section3',
+      title: t('start.sections.section3.title'),
+      content: [t('start.sections.section3.content')],
+      image: process.env.PUBLIC_URL + '/images/13.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section4',
+      title: t('start.sections.section4.title'),
+      content: [
+        t('start.sections.section4.content1'),
+        t('start.sections.section4.content2'),
+      ],
+      image: process.env.PUBLIC_URL + '/images/14.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section5',
+      title: t('start.sections.section5.title'),
+      content: [
+        t('start.sections.section5.content1'),
+        t('start.sections.section5.content2'),
+        t('start.sections.section5.content3'),
+      ],
+      image: process.env.PUBLIC_URL + '/images/15.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section6',
+      title: t('start.sections.section6.title'),
+      content: [t('start.sections.section6.content')],
+      image: process.env.PUBLIC_URL + '/images/12.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
+    {
+      id: 'section7',
+    title: t('start.sections.section7.title'),
+    content: [
+      t('start.sections.section7.content1'),
+      t('start.sections.section7.content2')      
+    ],
+    list: t('start.sections.section7.list', { returnObjects: true }),
+    contentAfterList: t('start.sections.section7.content3'),
+      image: process.env.PUBLIC_URL + '/images/14.jpg',
+      icon: process.env.PUBLIC_URL + '/images/icon.png',
+    },
   ];
 
   const sliderSettings = {
@@ -87,7 +145,7 @@ const StartPage = ({ reviews, specialists }) => {
         </div>
       </div>
       <div className="content">
-        {sections.map((section) => (
+        {sections.map(section => (
           <div key={section.id} className="section">
             <div className="section-image">
               <img src={section.image} alt={section.title} />
@@ -98,7 +156,17 @@ const StartPage = ({ reviews, specialists }) => {
                 <h2>{section.title}</h2>
               </div>
               <div className="section-content">
-                <p>{section.content}</p>
+                {section.content.map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+                {section.list && (
+                  <ul>
+                    {section.list.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                )}
+                {section.contentAfterList && <p>{section.contentAfterList}</p>}
               </div>
             </div>
           </div>
@@ -107,7 +175,9 @@ const StartPage = ({ reviews, specialists }) => {
       <div className="reviews-section">
         <Slider {...sliderSettings}>
           {reviews.map((review, index) => {
-            const specialist = specialists.find(spec => spec.id === review.specialistId);
+            const specialist = specialists.find(
+              spec => spec.id === review.specialistId
+            );
             return (
               <ReviewCard key={index} review={review} specialist={specialist} />
             );
